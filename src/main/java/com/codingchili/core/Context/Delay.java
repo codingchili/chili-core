eb6629e18b305ecf076b8573bf4c143feec311a4
chilimannen <rduda@kth.se>
2016-11-12 02:06:16 +0100

    Add JavaDoc to Delay

--- src/main/java/com/codingchili/core/Context/Delay.java
+++ src/main/java/com/codingchili/core/Context/Delay.java
@@ -4,10 +4,16 @@ import io.vertx.core.Future;
 
 /**
  * @author Robin Duda
+ *
+ * Delays given futures to allow for cleanup or to implement backoff timers.
  */
 public class Delay {
     private static CoreContext context;
 
+    /**
+     * Initializes the delay system with a core context.
+     * @param context containing timer implementation to use when delaying.
+     */
     public static void initialize(CoreContext context) {
         Delay.context = context;
     }
@@ -18,6 +24,10 @@ public class Delay {
         });
     }
 
+    /**
+     * Delays the given future with the shutdown-log timeout defined in system configuration.
+     * @param future the future to be delayed.
+     */
     public static void forShutdown(Future<Void> future) {
         Delay.future(future, context.system().getShutdownLogTimeout());
     }
