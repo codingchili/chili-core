6955a85c79bd6621877480ce31c6e04c7a4506f6
chilimannen <rduda@kth.se>
2017-03-02 06:42:05 +0100

    Wrap JsonObject to add support for Storable

---
+++ core/main/com/codingchili/core/storage/JsonItem.java
@@ -0,0 +1,24 @@
+package com.codingchili.core.storage;
+
+import io.vertx.core.json.JsonObject;
+
+/**
+ * @author Robin Duda
+ *         <p>
+ *         Extends the JsonObject making it storable.
+ */
+public class JsonItem extends JsonObject implements Storable {
+
+    public JsonItem() {
+    }
+
+    public JsonItem(JsonObject object) {
+        this.mergeIn(object);
+    }
+
+    @Override
+    public String id() {
+        return (this.containsKey(idField)) ?
+                this.getString(Storable.idField) : this.hashCode() + "";
+    }
+}
