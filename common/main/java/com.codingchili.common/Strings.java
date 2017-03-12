b0a3f71187f4c1d1012e41bb02dc0046481d288b
chilimannen <rduda@kth.se>
2017-03-12 20:56:33 +0100

    Add unique node name for realmregistry clients.

--- common/main/java/com.codingchili.common/Strings.java
+++ common/main/java/com.codingchili.common/Strings.java
@@ -60,7 +60,8 @@ public abstract class Strings extends CoreStrings {
     public static final String NODE_AUTHENTICATION_REALMS = "realm.authentication.node";
     public static final String NODE_WEBSERVER = "webserver.node";
     public static final String NODE_PATCHING = "patching.node";
-    public static final String NODE_REALM = ".realm.node";
+    public static final String NODE_REALM_CLIENTS = "client.realm.node";
+    public static final String NODE_REALM = "realm.node";
     public static final String NODE_CLIENT_LOGGING = "client.logging.node";
     public static final String NODE_ROUTING = "routing.node";
     public static final String SOCIAL_NODE = "social.node";
