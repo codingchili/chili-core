2ee0236c41391707bc08a1ced40d521921d15b1f
chilimannen <rduda@kth.se>
2017-03-31 06:51:17 +0200

    Fix invalid import in Realm/ConfigurationsTest

--- services/realm/test/java/com/codingchili/realm/model/ConfigurationsTest.java
+++ services/realm/test/java/com/codingchili/realm/model/ConfigurationsTest.java
@@ -5,7 +5,7 @@ import io.vertx.core.json.JsonArray;
 import io.vertx.core.json.JsonObject;
 import io.vertx.ext.unit.junit.Timeout;
 import io.vertx.ext.unit.junit.VertxUnitRunner;
-import io.vertx.rxjava.ext.unit.TestContext;
+import io.vertx.ext.unit.TestContext;
 import org.junit.*;
 import org.junit.runner.RunWith;
 
