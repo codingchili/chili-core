733f731c0d3893613a5b796cbed91fb0b68fd73e
Robin Duda <robin.duda@idainfront.se>
2018-05-01 15:40:12 +0200

    Add javadoc to com.codinghili.core.security.Token

--- core/main/java/com/codingchili/core/security/Token.java
+++ core/main/java/com/codingchili/core/security/Token.java
@@ -1,6 +1,7 @@
 package com.codingchili.core.security;
 
 import com.codingchili.core.files.Configurations;
+
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
 import java.io.Serializable;
@@ -10,7 +11,7 @@ import java.util.*;
 /**
  * @author Robin Duda
  * <p>
- * Used to authenticate requests between services.
+ * Used to authenticate requests between services and with clients.
  */
 public class Token implements Serializable {
     private Map<String, Object> properties = new HashMap<>();
@@ -19,36 +20,66 @@ public class Token implements Serializable {
     private long expiry = Instant.now().getEpochSecond() +
             Configurations.security().getTokenttl();
 
+    /**
+     * Creates a new empty unverified token.
+     */
     public Token() {
     }
 
+    /**
+     * Creates a new hmac token with the specified token factory.
+     *
+     * @param factory the token factory to generate the hmac.
+     * @param domain  a unique identifier - username or service id.
+     */
     public Token(TokenFactory factory, String domain) {
         this.domain = domain;
         factory.hmac(this);
     }
 
+    /**
+     * @return the cipher value of the token.
+     */
     public String getKey() {
         return key;
     }
 
+    /**
+     * @param key the cipher value, this should be cryptographically generated and balidated.
+     * @return fluent.
+     */
     public Token setKey(String key) {
         this.key = key;
         return this;
     }
 
+    /**
+     * @return the time of expiry in epoch seconds.
+     */
     public long getExpiry() {
         return expiry;
     }
 
+    /**
+     * @param expiry the time of expiry on epoch seconds.
+     * @return fluent
+     */
     public Token setExpiry(long expiry) {
         this.expiry = expiry;
         return this;
     }
 
+    /**
+     * @return the domain in which this token is valid for authentication.
+     */
     public String getDomain() {
         return domain;
     }
 
+    /**
+     * @param domain the domain where this token should be valid for authentication.
+     * @return fluent.
+     */
     public Token setDomain(String domain) {
         this.domain = domain;
         return this;
@@ -60,15 +91,29 @@ public class Token implements Serializable {
         return (T) properties.get(key);
     }
 
+    /**
+     * Adds a property to the token. This must be cryptographically incorporated in the {@link #getKey()}.
+     *
+     * @param key   the key of the property to set.
+     * @param value the value of the property to set.
+     * @return fluent.
+     */
     public Token addProperty(String key, Object value) {
         properties.put(key, value);
         return this;
     }
 
+    /**
+     * @return all properties that are set on the token.
+     */
     public Map<String, Object> getProperties() {
         return properties;
     }
 
+    /**
+     * @param properties a map of properties to set.
+     * @return fluent.
+     */
     public Token setProperties(Map<String, Object> properties) {
         this.properties = properties;
         return this;
